b37ea7335612e49328e5e7cb1ab8d8167a122c54
yonzheng-sh <yonzheng@stubhub.com>
2022-01-12 16:12:05 +0800

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -17,6 +17,6 @@ const Hello = (props) => (
 console.log("Listening on http://localhost:8080");
 serve((req) => {
   const url = new URL(req.url);
-  const name = url.searchParams.get("name") ?? "zhengyong";
+  const name = url.searchParams.get("name") ?? "zhengyong2";
   return ssr(() => <Hello name={name} />);
 });
